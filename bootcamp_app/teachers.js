835e9c9a4ff9d85e5dc99fffab9c4e8d3124c633
spiritxhx <spiritxhx@gmail.com>
2019-07-24 20:00:39 -0400

    use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,15 +9,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+// const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = ['%' + cohortName + '%'];
+
 pool.query(`
 select distinct teachers.name as teacher, cohorts.name as cohort
 from assistance_requests
 join teachers on teachers.id = teacher_id
 join students on students.id = student_id
 join cohorts on cohorts.id = cohort_id
-where cohorts.name LIKE '%${args[0]}%'
+where cohorts.name LIKE $1
 order by teacher;
-`)
+`, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort}: ${user.teacher}`);
